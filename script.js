a6c9f75655c2c780011ab77361e2fab56c2737cc
k0taro9 <126588189+k0taro9@users.noreply.github.com>
2023-11-09 10:39:36 +0100

    zahlraten fertig

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-let eingabe = prompt("Gebe eine Zahl zwischen 1 und 10 ein.");
+let min = 1;
+let max = 10;
+let eingabe;
+let randomNumber;
 
-if (eingabe <= 10 && eingabe >= 1) {
-    randomZahl;
+eingabe = Number(prompt(`Gebe eine Zahl zwischen ${min} und ${max} ein.`));
+if (eingabe >= min && eingabe <= max) {
+    randomNumber = getRandomNumber(min, max);
+    vergleichen(eingabe, randomNumber);
 }
 
-function randomZahl() {
-    
+function raten() {
+    eingabe = Number(prompt(`Gebe eine Zahl zwischen ${min} und ${max} ein.`));
+    if (eingabe >= min && eingabe <= max) {
+        vergleichen(eingabe, randomNumber);
+    }
+}
+
+function getRandomNumber(min = 1, max = 10) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function vergleichen(eingabe, randomNumber) {
+    if (eingabe === randomNumber) {
+        alert("Treffer");
+    } else if (eingabe > randomNumber) {
+        alert("Die Zahl ist zu gross.");
+        raten();
+    } else {
+        alert("Die Zahl ist zu klein.")
+        raten();
+    }
 }
